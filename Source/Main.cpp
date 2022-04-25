6849ad600cf06e35862286741c92f1db66be36dd
Shimrra Shai <shimmyshai00@gmail.com>
2022-04-24 22:49:12 -0700

    Get Main.cpp written with stub

---
+++ Source/Main.cpp
@@ -0,0 +1,14 @@
+// (C) 2020-2022. All rights reserved. See LICENSE for legal and redistribution terms.
+// 
+// AUTHOR:  Shimrra Shai
+// PROJECT: NeoIMP
+// FILE:    Main.cpp
+// PURPOSE: Implements the program main function.
+
+// FUNCTION: main
+// PURPOSE:  Provides the program entry point.
+// NOTES:    None.
+int
+main(int argc, char **argv) {
+    return 0;
+}
